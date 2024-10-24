a619b2ed59cc7f90ba034d6515184af131c77693
danielcstock <danielcstock12@gmail.com>
2024-10-24 01:07:25 -0300

    tests: add test files to project

---
+++ Tests/CustomerBllTest.cs
@@ -0,0 +1,11 @@
+namespace Tests;
+
+[TestClass]
+public class CustomerBllTest
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        Assert.AreEqual(1, 1);
+    }
+}
---
+++ Tests/OrderBllTest.cs
@@ -0,0 +1,11 @@
+namespace Tests;
+
+[TestClass]
+public class OrderBllTest
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        Assert.AreEqual(1, 1);
+    }
+}
