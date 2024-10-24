28e22aee93c8ec0aabeb2a3b9714b14712cb3bf4
danielcstock <danielcstock12@gmail.com>
2024-10-24 01:03:59 -0300

    refactor(order): refactor order object. Removed unused attributes

--- Model/Classes/Order.cs
+++ Model/Classes/Order.cs
@@ -7,7 +7,6 @@ public class Order
 {
     public int Id {get;set;}
     public DateTime Date {get;set;}
-    public int Status {get;set;}
-    public List<OrderItem> Items {get;set;}
+    public IList<OrderItem> Items {get;set;}
     public int CustomerId {get;set;}
 }
