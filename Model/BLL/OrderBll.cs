16af51f862bd2099c0ff01727d4926044e5fa09b
danielcstock <danielcstock12@gmail.com>
2024-10-24 01:03:26 -0300

    feat(order): add verification for open orders and handle non-existent order search

--- Model/BLL/OrderBll.cs
+++ Model/BLL/OrderBll.cs
@@ -6,51 +6,46 @@ using Model.Classes;
 public class OrderBll
 {
     public static async Task<IList<Order>> GetAllOrders(Context db){
-        var order = await db.Orders.ToArrayAsync();
-        var customerBll = new CustomerBll();
-        var orderItemBll = new OrderItemBll();
-
-        // order.Customer = customerBll.GetCustomer(order.CustomerId, db);
-        // order.Items = orderItemService.GetAllItemsFromOrder(order.Id, db);
-        return null;
-    }
-
-    public static async Task<IList<Order>> GetCompleteOrders(Context db){
-        return await db.Orders
-            .Where(order => order.Status == 3)
-            .ToArrayAsync();
+        return await db.Orders.ToArrayAsync();
     }
 
     public static async Task<Order> GetOrder(int id, Context db){
-        return await db.Orders.FindAsync(id)
-            is Order order
-                ? order
+        var order = await db.Orders.FindAsync(id)
+            is Order o
+                ? o
                 : null;
+
+        if(order == null){
+            throw new Exception("Order not found!");
+        }
+
+        var orderItemBll = new OrderItemBll();
+        order.Items = await OrderItemBll.GetAllItemsFromOrder(order.Id, db);
+
+        return order;
     }
 
-    public static async Task<IList<Order>> GetOrderByCustomerId(int customerId, Context db){
+    public static async Task<IList<Order>> GetAllOrdersByCustomerId(int customerId, Context db){
         return await db.Orders
             .Where(order => order.CustomerId == customerId)
             .ToArrayAsync();
     }
     
     public static async Task<Order> CreateOrder(Order order, Context db){
+        var customer = await CustomerBll.GetCustomer(order.CustomerId, db)
+            is Customer c
+                ? c
+                : null;
+
+        if(customer is null){
+            throw new Exception("Customer not found!");
+        }
+
         db.Orders.Add(order);
         db.OrderItems.AddRange(order.Items);
         await db.SaveChangesAsync();
 
         return order;
-    }
-
-    public static async Task UpdateOrder(int id, Order inputOrder, Context db){
-        var order = await db.Orders.FindAsync(id);
-
-        if (order is not null) {
-            // order.Buyer.Name = inputOrder.Buyer.Name;
-            order.Status = inputOrder.Status;
-
-            await db.SaveChangesAsync();
-        } 
     }
 
     public static async Task DeleteOrder(int id, Context db){
